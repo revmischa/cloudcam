f81821ccf3f0214d25cd1de2f647b407209a94bb
someone--else <mail@vlad.works>
2017-06-18 23:37:41 +0300

    Updated CloudFormation template/converted to YAML from JSON

--- cloudformation/deploy-stack.sh
+++ cloudformation/deploy-stack.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-MODE=${1:-update}              # create or update
+STACK_NAME=cloudcamdev         # stack name
 S3_CODE_BUCKET=cloudcam-code   # s3 bucket to upload lambda code to, will be created if doesn't exist
-STACK_NAME=cloudcam-test       # stack name
-IOT_CA_CERT_ID=dc6763eaf7b543310159727dee5232f973292b174a8cfa251713fbb3294797fe   # IoT CA AWS cert id used for just-in-time thing cert registration
+S3_UI_BUCKET=${STACK_NAME}-ui  # ui bucket
+
+MODE=${1:-update}              # create or update
 
 aws s3 mb s3://${S3_CODE_BUCKET}
-aws cloudformation package --template-file cloudcam.json --s3-bucket ${S3_CODE_BUCKET} --use-json --output-template-file cloudcam-packaged.json
-aws cloudformation ${MODE}-stack --template-body file://cloudcam-packaged.json --stack-name ${STACK_NAME} --capabilities=CAPABILITY_IAM --parameters ParameterKey=iotCaCertId,ParameterValue=${IOT_CA_CERT_ID}
+aws cloudformation package --template-file cloudcam.yml --s3-bucket ${S3_CODE_BUCKET} --output-template-file cloudcam-packaged.yml
+aws cloudformation ${MODE}-stack --template-body file://cloudcam-packaged.yml --stack-name ${STACK_NAME} --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+( cd ../dev-ui && NODE_ENV=production webpack )
+aws s3 cp --recursive --acl public-read ../dev-ui/webroot s3://${S3_UI_BUCKET}
