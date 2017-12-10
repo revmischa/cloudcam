5a92ad9cfd2d95ccad373c44d15e2c44d4f9ab49
someone--else <mail@vlad.works>
2017-12-10 03:49:52 +0300

    Rust client: RTP/RTSP support

--- client/rust/src/config.rs
+++ client/rust/src/config.rs
@@ -4,6 +4,7 @@ use serde_json;
 use serde_json::Error;
 
 fn default_vapix_username() -> String { "cloudcam".to_string() }
+
 fn default_vapix_password() -> String { "cloudcam".to_string() }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -21,6 +22,19 @@ impl Default for AxisConfig {
             vapix_username: default_vapix_username(),
             vapix_password: default_vapix_password(),
             snapshot_media_props: "".to_string()
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct RtspConfig {
+    #[serde(default)] pub uri: Option<String>,
+}
+
+impl Default for RtspConfig {
+    fn default() -> RtspConfig {
+        RtspConfig {
+            uri: None
         }
     }
 }
@@ -56,13 +70,14 @@ impl MqttTopicsConfig {
 pub struct Config {
     #[serde(rename = "thingName")] pub thing_name: String,
     #[serde(rename = "thingTypeName")] pub thing_type_name: String,
-    #[serde(rename = "clientId")] pub client_id: String,
-    pub endpoint: String,
+    #[serde(rename = "clientId")] pub mqtt_client_id: String,
+    #[serde(rename = "endpoint")] pub mqtt_endpoint: String,
     #[serde(rename = "caPem")] pub ca_pem: String,
     #[serde(rename = "certificatePem")] pub cert_pem: String,
     #[serde(rename = "certificatePrivateKey")] pub cert_private_key: String,
 
     #[serde(default)] pub axis: AxisConfig,
+    #[serde(default)] pub rtsp: RtspConfig,
 
     #[serde(skip)] pub mqtt_topics: MqttTopicsConfig
 }
@@ -71,7 +86,12 @@ impl Config {
     pub fn read(path: &Path) -> Result<Config, Error> {
         let f = File::open(path).expect("file not found");
         let mut config: Config = serde_json::from_reader(f).unwrap();
+        // assign mqtt topic names based on supplied thing name
         config.mqtt_topics = MqttTopicsConfig::new(&config.thing_name);
+        // append the default mqtt port to the endpoint if isn't there
+        if config.mqtt_endpoint.find(':') == None {
+            config.mqtt_endpoint = format!("{}:8883", config.mqtt_endpoint)
+        };
         return Ok(config);
     }
 }
--- client/rust/src/main.rs
+++ client/rust/src/main.rs
@@ -9,16 +9,24 @@ extern crate serde_derive;
 extern crate native_tls;
 extern crate hyper;
 extern crate hyper_tls;
-extern crate tokio_core;
 extern crate futures;
+extern crate tokio_io;
+extern crate tokio_core;
+extern crate tokio_proto;
+extern crate tokio_service;
+#[macro_use]
+extern crate nom;
+extern crate bytes;
+extern crate dns_lookup;
 
-use std::time::Duration;
-use std::thread;
 use rumqtt::{MqttOptions, MqttClient, QoS, MqttCallback, Message};
+use std::thread;
+use std::time::Duration;
 use std::path::Path;
 use std::sync::{Arc, Mutex};
 use std::option::Option;
 use std::str::from_utf8;
+use std::net::{IpAddr, Ipv4Addr};
 
 mod error;
 mod config;
@@ -27,8 +35,11 @@ mod axis_vapix;
 mod axis_native;
 mod upload_thumb;
 mod streams;
+mod rtsp;
+mod rtp;
 
 use config::Config;
+use rtp::RtpDest;
 
 fn handle_command(config: &Config, mqtt_client: &mut MqttClient, payload: &serde_json::Value) {
     payload.get("command").map(|command| {
@@ -49,7 +60,7 @@ fn handle_command(config: &Config, mqtt_client: &mut MqttClient, payload: &serde
             }
         }.map(|payload| {
             // if the handler above returned a shadow update document, publish it to the appropriate topic
-            info!("command {} state update: {:?}", command, from_utf8(&*payload));
+            info!("command {} state update: {:?}", command, from_utf8(payload.as_ref()));
             mqtt_client.publish(config.mqtt_topics.shadow_update.as_str(), QoS::Level1, payload).unwrap_or_else(|e| {
                 error!("error publishing command state update: {}", e);
             });
@@ -57,13 +68,13 @@ fn handle_command(config: &Config, mqtt_client: &mut MqttClient, payload: &serde
     });
 }
 
-fn handle_shadow_update_delta(config: &Config, mqtt_client: &mut MqttClient, payload: &serde_json::Value) {
-    payload.get("state").map(|state| {
-        state.as_object().map(|state| {
-            for (key, value) in state {
+fn handle_shadow_update_delta(config: &Config, mqtt_client: &mut MqttClient, rtp_dest: &Mutex<RtpDest>, payload: &serde_json::Value) {
+    payload.get("state").map(|shadow_state| {
+        shadow_state.as_object().map(|shadow_state| {
+            for (key, value) in shadow_state {
                 match key.as_str() {
                     "streams" => {
-                        streams::state_update(config, value).unwrap_or_else(|e| {
+                        streams::state_update(config, value, rtp_dest).unwrap_or_else(|e| {
                             error!("streams::state_update() error: {:?}", e);
                             None
                         })
@@ -74,7 +85,7 @@ fn handle_shadow_update_delta(config: &Config, mqtt_client: &mut MqttClient, pay
                     }
                 }.map(|payload| {
                     // if the handler above returned a shadow update document, publish it to the appropriate topic
-                    info!("shadow update handler {} state update: {:?}", key, from_utf8(&*payload));
+                    info!("shadow update handler {} state update: {:?}", key, from_utf8(payload.as_ref()));
                     mqtt_client.publish(config.mqtt_topics.shadow_update.as_str(), QoS::Level1, payload).unwrap_or_else(|e| {
                         error!("error publishing shadpw update handler state update: {}", e);
                     });
@@ -86,16 +97,12 @@ fn handle_shadow_update_delta(config: &Config, mqtt_client: &mut MqttClient, pay
 
 fn main() {
     env_logger::init().unwrap();
+    // global state which could be updated by commands/shadow delta handlers
+    let rtp_dest = Arc::new(Mutex::<RtpDest>::new(RtpDest { addresses: None }));
     // read config.json
     let config = Config::read(Path::new("./config.json")).expect("no or malformed config.json");
-    // append the default mqtt port to the endpoint if isn't there
-    let iot_endpoint = if config.endpoint.find(':') == None {
-        format!("{}:8883", config.endpoint)
-    } else {
-        config.endpoint.clone()
-    };
-    info!("iot endpoint: {iot_endpoint}, thing name: {thing_name}, client id: {client_id}",
-          iot_endpoint = iot_endpoint, thing_name = config.thing_name, client_id = config.client_id);
+    info!("iot endpoint: {endpoint}, thing name: {thing_name}, client id: {client_id}",
+          endpoint = config.mqtt_endpoint, thing_name = config.thing_name, client_id = config.mqtt_client_id);
     // write tls certs/keys to temporary files for the mqtt client
     let ca_path = Path::new("./cloudcam-ca.pem");
     let cert_path = Path::new("./cloudcam.crt");
@@ -105,27 +112,29 @@ fn main() {
     tools::spit(key_path, &config.cert_private_key);
     // configure mqtt client
     let client_options = MqttOptions::new()
+        .set_client_id(config.mqtt_client_id.clone())
+        .set_clean_session(true) // AWS IoT broker only supports clean sessions
         .set_keep_alive(5)
-        .set_reconnect(3)
-        .set_client_id(config.client_id.clone())
+        .set_reconnect(3) // seconds between reconnect attempts
         .set_ca(ca_path)
         .set_client_cert(cert_path, key_path)
-        .set_broker(iot_endpoint.as_str());
-    // setup a mutex-protected reference to optionally present mqtt client so we can pass it to callback before creating the client itself
-    let mqtt_client_ref = Arc::new(Mutex::<Option<Box<MqttClient>>>::new(None));
+        .set_broker(config.mqtt_endpoint.as_str());
+    // reference to optionally present mqtt client so we can pass it to callback before creating the client itself
+    let mqtt_client_ref = Arc::new(Mutex::<Option<MqttClient>>::new(None));
     let msg_callback = {
         let mqtt_client_ref = mqtt_client_ref.clone();
         let config = config.clone();
+        let rtp_dest = rtp_dest.clone();
         // a callback to be called upon receiving any mqtt message
         MqttCallback::new().on_message(move |message: Message| {
             match *mqtt_client_ref.lock().unwrap() {
                 Some(ref mut mqtt_client) => {
-                    serde_json::from_slice(&*message.payload).map(|json_value: serde_json::Value| {
+                    serde_json::from_slice(message.payload.as_ref()).map(|json_value: serde_json::Value| {
                         let topic = message.topic.as_str();
                         if topic == config.mqtt_topics.commands {
                             handle_command(&config, mqtt_client, &json_value);
                         } else if topic == config.mqtt_topics.shadow_delta {
-                            handle_shadow_update_delta(&config, mqtt_client, &json_value);
+                            handle_shadow_update_delta(&config, mqtt_client, &rtp_dest, &json_value);
                         } else {
                             error!("unrecognized message: topic: {:?}, payload: {:?}", message.topic, json_value.to_string());
                         };
@@ -140,7 +149,7 @@ fn main() {
     // start the mqtt client, put it into the mutex-protected reference (see above) and subscribe to the topics we want
     {
         let mut mqtt_client_guard = mqtt_client_ref.lock().unwrap();
-        *mqtt_client_guard = Some(Box::new(MqttClient::start(client_options, Some(msg_callback)).expect("coudn't start mqtt client")));
+        *mqtt_client_guard = Some(MqttClient::start(client_options, Some(msg_callback)).expect("coudn't start mqtt client"));
         match *mqtt_client_guard {
             Some(ref mut mqtt_client) => {
                 let topics = vec![(config.mqtt_topics.commands.as_str(), QoS::Level1),
@@ -151,8 +160,33 @@ fn main() {
             None => panic!("no mqtt client")
         };
     }
+
+    // if rtsp is configured, start a session and rtp reflector
+    match config.rtsp.uri {
+        Some(rtsp_uri) => {
+            // setup rtsp session/rtp reflector
+            let rtp_recv_ports = rtp::find_port_pair((40000..50000)).unwrap();
+            match rtsp::start_session(rtsp_uri.as_str(), rtp_recv_ports) {
+                Ok(rtsp_session) => {
+                    // run a keepalives thread so rtsp session doesn't timeout
+                    std::thread::spawn(move || {
+                        rtsp_session.run_keepalives();
+                    });
+                    // run rtp reflector to send rtp packet received locally to the janus gateway
+                    std::thread::spawn(move || {
+                        // todo: listen on 127.0.0.1 instead of 0.0.0.0 if RTSP session is local-only (e.g. running on camera)
+                        rtp::run_reflector(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), rtp_recv_ports, &rtp_dest);
+                    });
+                }
+                Err(e) => error!("rtsp session error: {:?}", e)
+            }
+        }
+        None => ()
+    }
+
     // now await the heat death of the universe
     loop {
+        // todo: implement a watchdog timer
         thread::sleep(Duration::new(1, 0));
     }
 }
